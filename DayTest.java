8b190cf4e22ee56a7008d62d88d68b8cd9bd571b
daanLockhorst <daanlockhorst@hotmail.com>
2018-06-15 14:23:13 +0200

    Finised the code till exercise 6

--- DayTest.java
+++ DayTest.java
@@ -19,7 +19,6 @@ public class DayTest
     * appointment made at nine.
     * 
     */
-  
     @Test
     public void testFindSpaceWithFirstSlotFilled()
     {
@@ -35,7 +34,7 @@ public class DayTest
         System.out.println("test failure: the number is not 10 it's "
             + testResult);
             }
-}
+        }
     /**
     * This method testst if the findSpace returns -1 when the whole 
     * day is booked full
@@ -60,6 +59,31 @@ public class DayTest
             System.out.println("test failed");
         }
     }
+    /**
+    * This method testst if the findSpace returns -1 when the day is booked
+    * fully. But improved doesnt use loops but instead one 9 hour appointment
+    * 
+    */
+    @Test 
+    public void testFindSpaceImproved()
+    {
+        Day d = new Day(4);
+        Appointment a = new Appointment("stuff", 9);
+        Appointment b = new Appointment("moreStuff", 1);
+        d.makeAppointment(9, a);
+        int result = d.findSpace(b);
+        if(result == -1)
+        {
+            System.out.println("test succes: returned -1");
+        } else{
+            System.out.println("test failed" + result);
+        }
+    }
+    /**
+    * Makes a test that breaks aka the wrong day for veganfood
+    * 
+    * 
+    */
     @Test 
     public void testThatBreaks()
     {
@@ -71,5 +95,29 @@ public class DayTest
         theWrongDayForVeganFood.makeAppointment(9, ap1);
         System.out.println("test failure: returns " +
         theWrongDayForVeganFood.makeAppointment(9, ap2));
-}
+    }
+    /**
+    * Some extra test that test all the other functions in the day class
+    * one test that runs and one that breakss
+    */
+    @Test 
+    public void testShowAppointments()
+    {
+        Day busyDay = new Day(1);
+        Day calmDay = new Day(2);
+        Appointment one = new Appointment("info1", 6);
+        Appointment two = new Appointment("GWP", 2);
+        busyDay.makeAppointment(9, one);
+        calmDay.makeAppointment(11, two);
+        busyDay.showAppointments();
+        calmDay.showAppointments();
+    }
+    @Test
+    public void extraTestThatBreaksStuff()
+    {
+        Day shittyDay = new Day(3);
+        Appointment a = new Appointment("Putzen", 1);
+        shittyDay.makeAppointment(9, a);
+        System.out.println(shittyDay.getAppointment(9));
+    }
 }
